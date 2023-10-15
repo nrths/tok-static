539ac0e621922c77da00a878c144232efd0e320c
nrths <northnosouth@gmail.com>
2023-10-15 06:38:33 +0300

    customer edits

--- pages/products/[title].tsx
+++ pages/products/[title].tsx
@@ -15,7 +15,7 @@ const ProductPage: NextPage = ({ item }: any) => {
       seoTitle = `Кровать ${item.name.toUpperCase()} | TOK`;
     } else if (category === "SOFA") {
       seoTitle = `Диван ${item.name.toUpperCase()} | TOK`;
-    } else if (category.includes("TABLE_DINNER")) {
+    } else if (category.includes("CIRCLE")) {
       seoTitle = `Круглый стол ${item.name.toUpperCase()} | TOK`;
     } else if (category.includes("TABLE_DINNER LONG")) {
       seoTitle = `Обеденный стол ${item.name.toUpperCase()} | TOK`;
